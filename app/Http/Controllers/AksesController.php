0cb34c8804b977fd1d62ffe530bc60c139fa1828
niluhputu22 <niluhputusriastiti5@gmail.com>
2021-04-02 13:31:49 +0800

    update

---
+++ app/Http/Controllers/AksesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AksesController extends Controller
+{
+    public function index()
+    {
+        return view('identitas');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,272 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <title>Portofolio</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+  <link href="porto/assets/img/unnamed.png" rel="icon">
 
-            .full-height {
-                height: 100vh;
-            }
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  <link href="porto/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="porto/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="porto/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="porto/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="porto/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="porto/assets/vendor/aos/aos.css" rel="stylesheet">
 
-            .position-ref {
-                position: relative;
-            }
+  <link href="porto/assets/css/style.css" rel="stylesheet">
+</head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<body>
 
-            .content {
-                text-align: center;
-            }
+  <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
 
-            .title {
-                font-size: 84px;
-            }
+  <header id="header">
+    <div class="d-flex flex-column">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+      <div class="profile">
+        <img src="porto/assets/img/foto.jpg" alt="" width="100px" class="img-fluid rounded">
+        <div class="social-links mt-3 text-center">
+          <a href="http://twitter.com" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="http://facebook.com" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://instagram.com" class="instagram"><i class="bx bxl-instagram"></i></a>
         </div>
-    </body>
+      </div>
+
+      <nav class="nav-menu">
+        <ul>
+          <li><a href="#home"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#about"><i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#resume"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+          <li><a href="#contact"><i class="bx bx-envelope"></i> Contact</a></li>
+        </ul>
+      </nav>
+      <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+    </div>
+  </header>
+
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Ni Luh Putu Sri Astiti</h1>
+      <p>I'm <span class="typed" data-typed-items="Developer, Photographer"></span></p>
+    </div>
+  </section>
+
+  <main id="main">
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Biodata</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="porto/assets/img/foto.jpg" width="150px" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                <li><i class="icofont-rounded-right"></i> <strong>Lahir :</strong> 5 Desember 1999</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Umur :</strong> 21</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Agama :</strong> Hindu</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Status :</strong> Mahasiswi</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Jenis Kelamin :</strong> Perempuan</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Kewarganegaraan :</strong> Indonesia</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Kompetensi :</strong> Rekayasa Perangkat Lunak</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="skills" class="skills section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Skills</h2>
+        </div>
+
+        <div class="row skills-content">
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">HTML <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">85%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Java <i class="val">95%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">VB Net <i class="val">85%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">PHP <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Adobe Illustrator <i class="val">95%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Pendidikan</h3>
+            <div class="resume-item">
+            <h4>TK Pertiwi Cakra</h4>
+              <h5>2005-2006</h5>
+              <p><em>Siswa</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>SD Negeri 11 Cakranegara</h4>
+              <h5>2006-2012</h5>
+              <p><em>Siswa</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>SMP Negeri 4 Mataram</h4>
+              <h5>2012 - 2015</h5>
+              <p><em>Siswa</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>SMA Negeri 6 Mataram</h4>
+              <h5>2015-2018</h5>
+              <p><em>Siswa</em></p>
+            </div>
+             <div class="resume-item">
+              <h4>Universitas Bumigora</h4>
+              <h5>2018-Sekarang</h5>
+              <p><em>Mahasiswi</em></p>
+            </div>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Pengalaman </h3>
+            <div class="resume-item">
+              <h4>Organisasi Osis</h4>
+              <h5>2013 - 2015</h5>
+              <p><em>SMP Negeri 4 Mataram</em></p>
+              <ul>
+                <li>Mendistribusikan surat pelaksanaan kegiatan</li>
+                <li>Menyiapkan laporan dan evaluasi kegiatan</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Staff Administrasi</h4>
+              <h5>2016 - 2018</h5>
+              <p><em>Bengkel Mobil Astiti Karya Motor</em></p>
+              <ul>
+                <li>Membuat catatan dan laporan keuangan</li>
+                <li>Mengelola peralatan administrasi </li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Sekretaris UKM MAC</h4>
+              <h5>2019 - 2020</h5>
+              <p><em>Universitas Bumigora mataram</em></p>
+              <ul>
+                <li>Menyiapkan surat izin untuk peminjaman ruangan</li>
+                <li>Membuat jadwal rapat pengurus dan anggota</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="icofont-google-map"></i>
+                <h4>Lokasi</h4>
+                <p>Cakranegara, Mataram, Nusa Tenggara Barat</p>
+              </div>
+
+              <div class="email">
+                <i class="icofont-envelope"></i>
+                <h4>Email</h4>
+                <p>astitimega@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="icofont-phone"></i>
+                <h4>Telepon: </h4>
+                <p>083114305506</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <script src="porto/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="porto/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="porto/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="porto/assets/vendor/php-email-form/validate.js"></script>
+  <script src="porto/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="porto/assets/vendor/counterup/counterup.min.js"></script>
+  <script src="porto/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="porto/assets/vendor/venobox/venobox.min.js"></script>
+  <script src="porto/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="porto/assets/vendor/typed.js/typed.min.js"></script>
+  <script src="porto/assets/vendor/aos/aos.js"></script>
+
+  <script src="porto/assets/js/main.js"></script>
+
+</body>
 </html>
